2131c6af1040a8b31c0b446f18c6ddccc9ee79ee
th3r00t <admin@mylt.dev>
2020-09-15 23:24:20 -0400

    Slide updates

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,75 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="banner">
+      <b-carousel
+        fade
+        id="carousel-fade"
+        :interval="8000"
+        style="text-shadow: 0px 0px 2px #000"
+        img-width="1024px"
+        img-height="480px"
+      >
+        <b-carousel-slide
+          caption-html="
+            <h3 class='primary slide-hdr'>A Lightweight EBook Server</h3>
+            <h6 class='primary slide-sub-hdr'>pyShelf goes anywhere you can deploy a docker image</h6>
+            <p class='quote'>After some redesign pyShelf is now being developed with docker in mind</p>
+          "
+          img-src="./assets/slide-home-no-blur.png"
+        ></b-carousel-slide>
+        <b-carousel-slide
+          caption-html="
+            <h3 class='primary slide-hdr'>Powered By Python</h1>
+            <h6 class='primary slide-sub-hdr'>A vibrant and diverse eco-system, combined with sensible syntax made Python no-brainer for pyShelf</h6>
+          "
+          img-src="./assets/slide-dev-screenshot-no-blur.png"
+        ></b-carousel-slide>
+        <b-carousel-slide
+          caption-html="
+            <h3 class='primary slide-hdr'>Enjoying pyShelf?</h1>
+            <h6 class='primary slide-sub-hdr'>Consider Sponsoring On GitHub!</h6>
+            <p class='quote'>Sponsoring an Open Source project helps to ensure the future of Open Source Software!</p>
+          "
+          img-src="./assets/slide-githubsponsors-no-blur.png"
+        ></b-carousel-slide>
+        <div id="top-bar" class="d-flex justify-content-between">
+          <div id="branding" class="p2">
+            <div class="d-flex flex-column">
+              <div id="site-title" class="pl-1 text-left text-uppercase">
+                <h3 class="primary">pyShelf</h3>
+              </div>
+              <div id="site-slogan" class="pl-1 justify-text-top"><h6 class="font-weight-light">FOSS EBook Server</h6></div>
+            </div>
+          </div>
+          <div id="nav" class="pr-1 align-items-center mt-auto mb-auto">
+            <div class="mt-0 sticky-top p2">
+              <b-button-group size="md">
+                <router-link to="/">
+                  <b-button size="md" class="site-nav">
+                    <b-icon icon="house-door" aria-hidden="true"></b-icon> Home
+                  </b-button>
+                </router-link>
+                <router-link to="/download">
+                  <b-button size="md" class="site-nav">
+                    <b-icon icon="download" aria-hidden="true"></b-icon> Download
+                  </b-button>
+                </router-link>
+                <b-button size="md" class="site-nav">
+                  <!--<router-link to="/download">-->
+                  <b-icon icon="hdd-rack" aria-hidden="true"></b-icon> Get
+                  Involved
+                  <!--</router-link>-->
+                </b-button>
+              </b-button-group>
+            </div>
+          </div>
+        </div>
+      </b-carousel>
     </div>
     <router-view />
+    <div class="footer-container">
+      Footer
+    </div>
   </div>
 </template>
 
@@ -14,19 +79,48 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #000;
 }
-
+#banner {
+  background-size: cover;
+  background-blend-mode: luminosity;
+  background-color: #0000008c;
+  border-bottom: 10px solid #00000070;
+  border-top: 10px solid #00000070;
+  margin-bottom: 0px;
+}
+#top-bar {
+  //background-image: url("assets/blue-background.jpg");
+  background-color: #000000bd;
+  color: silver;
+  opacity: 90%;
+  position: fixed;
+  top: 0px;
+  min-width: 100%;
+  z-index: 1;
+}
 #nav {
-  padding: 30px;
+  padding: 0px;
 
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: #ffffff;
 
     &.router-link-exact-active {
-      color: #42b983;
+      color: #000000;
     }
   }
+}
+.site-nav {
+  background-color: #343a40;
+}
+.carousel-item {
+  height: 480px;
+  width: 100%;
+}
+.img-fluid {
+  max-width: 100%;
+  height: max-content;
+  image-rendering: pixelated;
 }
 </style>
---
+++ src/components/FrontPage.vue
@@ -0,0 +1,165 @@
+<template xmlns:a="http://www.w3.org/1999/html">
+  <div class="outwrap">
+    <div class="section first-section">
+      <div class="section-intro">
+        <h6 class="primary section-hdr">Feature Overview</h6>
+        <h3 class="section-sub-hdr">pyShelf is constantly evolving</h3>
+      </div>
+      <div class="section-container">
+        <b-container fluid="lg">
+          <b-row align-v="top" align-h="center" cols="4">
+            <b-col>
+              <div class="highlight-box">
+                <div class="highlight-inner">
+                  <img class="highlight-image" src="@/assets/docker.png"/>
+                  <div class="highlight-caption">
+                    <h5 class="primary">You asked for it</h5> It's here. pyShelf has been redesigned with Docker in mind
+                  </div>
+                </div>
+              </div>
+            </b-col>
+            <b-col>
+              <div class="highlight-box">
+                <div class="highlight-inner">
+                  <img class="highlight-image" src="@/assets/home-example.png"/>
+                  <div class="highlight-caption">
+                    <h5 class="primary">Web Based Interface</h5>
+                    Use pyShelf anywhere.
+                  </div>
+                </div>
+              </div>
+            </b-col>
+            <b-col>
+              <div class="highlight-box">
+                <div class="highlight-inner">
+                  <img class="highlight-image" src="@/assets/responsive-ui.png"/>
+                  <div class="highlight-caption">
+                    <h5 class="primary">Responsive U/X</h5>
+                    pyShelf is fully responsive, use it on any device
+                  </div>
+                </div>
+              </div>
+            </b-col>
+            <b-col>
+              <div class="highlight-box">
+                <div class="highlight-inner">
+                  <img class="highlight-image" src="@/assets/navbar.png"/>
+                  <div class="highlight-caption">
+                    <h5 class="primary">Streamlined U/I</h5>
+                    The interface has been redesigned for a more informative, less intrusive user experience.
+                  </div>
+                </div>
+              </div>
+            </b-col>
+            <b-col>
+              <div class="highlight-box">
+                <div class="highlight-inner">
+                  <img class="highlight-image" src="@/assets/cli-preview.png"/>
+                  <div class="highlight-caption">
+                    <h5 class="primary">T.U.I</h5>
+                    Terminal User Interface, pyShelf can be run as a headless service, no X server needed.
+                  </div>
+                </div>
+              </div>
+            </b-col>
+            <b-col>
+              <div class="highlight-box">
+                <div class="highlight-inner">
+                  <img class="highlight-image" src="@/assets/book-description.png"/>
+                  <div class="highlight-caption">
+                    <h5 class="primary">Metadata Retrieval</h5>
+                    We do our best to retrieve your books metadata.
+                  </div>
+                </div>
+              </div>
+            </b-col>
+            <b-col>
+              <div class="highlight-box">
+                <div class="highlight-inner">
+                  <img class="highlight-image" src="@/assets/book-details.png"/>
+                  <div class="highlight-caption">
+                    <h5 class="primary">Detailed View</h5>
+                    Want more details before choosing a book? Click the info button.
+                  </div>
+                </div>
+              </div>
+            </b-col>
+            <b-col>
+              <div class="highlight-box">
+                <div class="highlight-inner">
+                  <img class="highlight-image" src="@/assets/django.png"/>
+                  <div class="highlight-caption">
+                    <h5 class="primary">Django Driven</h5>
+                    When you need a dependable frontend framework look no further.
+                  </div>
+                </div>
+              </div>
+            </b-col>
+          </b-row>
+        </b-container>
+      </div>
+    </div>
+    <div class="section">
+      <div class="section-container">
+        <b-container>
+          <b-row align-v="top" align-h="center">
+            <b-col>
+              <div class="ad-box">
+                <div class="highlight-inner">
+                  <p>get started</p>
+                  <p>pyShelf is hosted on GitHub</p>
+                  <img src="@/assets/GitHub-Mark-64px.png" /><img src="@/assets/GitHub_Logo.png" width="320px"/><br />
+                </div>
+              </div>
+            </b-col>
+            <b-col>
+              <div class="ad-box">
+                <p>donate to pyShelf</p>
+                <p>Consider Sponsoring pyShelf on github to help with development</p>
+                <img src="@/assets/githubsponsors.png" width="320px" /><br />
+              </div>
+            </b-col>
+            <b-col>
+              <div class="ad-box">
+                <p>get involved</p>
+                <p>join us on discord</p>
+                <img src="@/assets/discord_logo_color.png" width="320px" /><br />
+              </div>
+            </b-col>
+          </b-row>
+        </b-container>
+      </div>
+      <div>
+      </div>
+      <div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FrontPage",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
